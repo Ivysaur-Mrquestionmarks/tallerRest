f7978f1eb203a7fcb2d97637a959f816be932a77
Ivysaur-Mrquestionmarks <75641314+Ivysaur-Mrquestionmarks@users.noreply.github.com>
2025-09-18 16:57:20 -0500

    First push

---
+++ src/main/java/com/eafit/tutorial/Artifact/products_api/ArtifactProductsApiApplication.java
@@ -0,0 +1,13 @@
+package com.eafit.tutorial.Artifact.products_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtifactProductsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtifactProductsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eafit/tutorial/Artifact/products_api/ArtifactProductsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eafit.tutorial.Artifact.products_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtifactProductsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
