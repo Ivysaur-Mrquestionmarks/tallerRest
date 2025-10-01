d458c0bd42d3e3384a7826e20f91ba5441d41882
Ivysaur-Mrquestionmarks <75641314+Ivysaur-Mrquestionmarks@users.noreply.github.com>
2025-10-01 13:09:27 -0500

    Update ApiResponse.java

--- src/main/java/com/eafit/tutorial/dto/ApiResponse.java
+++ src/main/java/com/eafit/tutorial/dto/ApiResponse.java
@@ -55,6 +55,10 @@ public class ApiResponse<T> {
         return new ApiResponse<>(false, message, null, statusCode);
     }
 
+    public static <T> ApiResponse<T> error(String message, int statusCode) {
+        return new ApiResponse<>(false, message, null, statusCode);
+    }    
+
     public static <T> ApiResponse<T> error(String message) {
         return new ApiResponse<>(false, message, null, 500);
     }
